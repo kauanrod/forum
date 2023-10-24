d4884ceaf087fec58c3161abfd67d979757d1fb1
Kauan Rodrigues <kauanrod@hotmail.com>
2023-10-24 18:50:27 -0300

    PUT and DELETE - CRUD Completed

--- src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
+++ src/main/kotlin/br/com/alura/forum/controller/TopicoController.kt
@@ -1,12 +1,15 @@
 package br.com.alura.forum.controller
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.service.TopicoService
 import jakarta.validation.Valid
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -26,7 +29,17 @@ class TopicoController(private val service: TopicoService) {
   }
 
   @PostMapping
-  fun cadastrar(@RequestBody @Valid dto: NovoTopicoForm) {
-    service.cadastrar(dto)
+  fun cadastrar(@RequestBody @Valid form: NovoTopicoForm) {
+    service.cadastrar(form)
+  }
+
+  @PutMapping
+  fun atualizar(@RequestBody @Valid form: AtualizacaoTopicoForm) {
+    service.atualizar(form)
+  }
+
+  @DeleteMapping("/{id}")
+  fun deletar(@PathVariable id: Long) {
+    service.deletar(id)
   }
 }
--- src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
+++ src/main/kotlin/br/com/alura/forum/service/TopicoService.kt
@@ -1,5 +1,6 @@
 package br.com.alura.forum.service
 
+import br.com.alura.forum.dto.AtualizacaoTopicoForm
 import br.com.alura.forum.dto.NovoTopicoForm
 import br.com.alura.forum.dto.TopicoView
 import br.com.alura.forum.mapper.TopicoFormMapper
@@ -34,5 +35,30 @@ class TopicoService(
     val topico = topicoFormMapper.map(form)
     topico.id = topicos.size.toLong() + 1
     topicos = topicos.plus(topico)
+  }
+
+  fun atualizar(form: AtualizacaoTopicoForm) {
+    val topico = topicos.stream().filter { t ->
+      t.id == form.id
+    }.findFirst().get()
+    topicos = topicos.minus(topico).plus(
+      Topico(
+        id = form.id,
+        titulo = form.titulo,
+        mensagem = form.mensagem,
+        usuario = topico.usuario,
+        curso = topico.curso,
+        respostas = topico.respostas,
+        status = topico.status,
+        dataCriacao = topico.dataCriacao
+      )
+    )
+  }
+
+  fun deletar(id: Long) {
+    val topico = topicos.stream().filter { t ->
+      t.id == id
+    }.findFirst().get()
+    topicos = topicos.minus(topico)
   }
 }
