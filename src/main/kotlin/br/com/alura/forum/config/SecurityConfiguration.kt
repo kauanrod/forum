12dab755be5fbcc0d11b61737a9274da06b3655c
Kauan Rodrigues <kauanrod@hotmail.com>
2024-03-05 21:41:24 -0300

    Auth with roles

--- src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
+++ src/main/kotlin/br/com/alura/forum/config/SecurityConfiguration.kt
@@ -2,9 +2,11 @@ package br.com.alura.forum.config
 
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
 import org.springframework.security.config.Customizer
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.invoke
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
@@ -15,14 +17,23 @@ import org.springframework.security.web.SecurityFilterChain
 class SecurityConfiguration {
   @Bean
   fun securityFilterChain(http: HttpSecurity): SecurityFilterChain {
-    return http
-      .csrf { it.disable() }
-      .authorizeHttpRequests {
-        it.anyRequest().authenticated()
+    http.invoke {
+      csrf { disable() }
+      authorizeRequests {
+        authorize(HttpMethod.GET,"/topicos", hasAuthority("LEITURA_ESCRITA"))
+        authorize(HttpMethod.POST,"/topicos", hasAuthority("LEITURA_ESCRITA"))
+        authorize(HttpMethod.PUT,"/topicos", hasAuthority("LEITURA_ESCRITA"))
+        authorize(HttpMethod.DELETE,"/topicos", hasAuthority("LEITURA_ESCRITA"))
+        
+        authorize(anyRequest, authenticated)
       }
-      .sessionManagement {
-        it.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-      }.httpBasic(Customizer.withDefaults()).build()
+      sessionManagement {
+        sessionCreationPolicy = SessionCreationPolicy.STATELESS
+      }
+      headers { frameOptions { disable() } }
+      httpBasic { }
+    }
+    return http.build()
   }
   
   @Bean
